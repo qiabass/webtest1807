4ee9e0aa6a2eef4be4368dc15c8360b046afca93
wugui <qiabass@163.com>
2018-07-10 11:29:15 +0800

    add main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+/* 
+* @Author: Marte
+* @Date:   2018-07-10 11:27:52
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-07-10 11:28:37
+*/
+
+$(document).ready(function(){
+    document.write("hello git world");
+});
